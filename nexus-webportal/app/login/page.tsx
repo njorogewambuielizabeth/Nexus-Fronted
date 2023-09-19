37dae7ec31b37ec9d2cc702d998cc8a15e5f1c80
Rachel Tuyishimire <rachelltuyishimire@gmail.com>
2023-09-19 21:42:38 +0300

    Edited the login page

--- nexus-webportal/app/login/page.tsx
+++ nexus-webportal/app/login/page.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 
+
+
 const Login = () => {
   return (
     <div className="bg-blue-800">
-         <div className="p-2">
-        <img className="w-20 h-20 absolute top-0 items-center mt-4 ml-4" src="./nexus.png" alt="Nexus Logo" />
-      </div>
+       {/* <div className="min-h-screen flex items-center justify-center">
+  <div className="p-2">
+    <img className="w-30 h-20" src="./logo.png" alt="Nexus Logo" />
+  </div>
+</div> */}
+
     <div className="h-screen flex flex-col">
      
       <div className="flex-1 flex justify-center items-center">
@@ -30,13 +35,24 @@ const Login = () => {
               placeholder="Password"
             />
            
-            <p className="text-center">Remember Me</p>
+           <div className="flex  ml-0">
+  <input type="checkbox" className="mr-2" />
+  <p className="text-center">Remember Me</p>
+</div>
+
             <br/>
             <button
   className="w-1/2 bg-blue-800 flex justify-center text-white p-3 rounded-full hover:bg-blue-700 text-xl font-bold mx-auto"
   type="submit"
 >
   Login
+</button>
+<br/>
+          <button
+  className="w-1/2 bg-blue-100 flex justify-center text-black p-3 rounded-full hover:bg-blue-50 text-m mb-2 mx-auto"
+  type="submit"
+>
+ <img className="w-5 mr-1 " src="./GoogleOutlined.png"/>  Continue with Google
 </button>
 
           </form>
--- nexus-webportal/app/register/page.tsx
+++ nexus-webportal/app/register/page.tsx
@@ -3,13 +3,13 @@ import React from "react";
 const Register = () => {
   return (
     <div className="bg-blue-800">
-         <div className="p-2">
-        {/* <img className="w-20 h-20 absolute top-0 items-center mt-4 ml-4" src="./nexus.png" alt="Nexus Logo" /> */}
+         <div className="p-2 flex items-center justify-center">
+        <img className="w-50 h-20 absolute top-0 items-center mt-4 ml-6" src="./logo.png" alt="Nexus Logo" />
       </div>
     <div className="h-screen flex flex-col">
      
       <div className="flex-1 flex justify-center items-center">
-        <div className="bg-white p-4 rounded shadow-md">
+        <div className="bg-white p-4 mb-20 rounded shadow-md">
         <h2 className="text-3xl font-bold text-blue-800 text-center">Register</h2>
 
             <br/>
@@ -45,7 +45,7 @@ const Register = () => {
               type="password"
               placeholder="Confirm Password"
             />
-            <p className="text-center">Already a member? <a className="text-blue-500" href="">Login</a></p>
+            <p className="text-center">Already a member? <a className="text-blue-500" href="#register">Login</a></p>
             <br/>
             <button
   className="w-1/2 bg-blue-800 flex justify-center text-white p-3 rounded-full hover:bg-blue-700 text-xl font-bold mx-auto"
